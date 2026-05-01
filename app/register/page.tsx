339f309ad3dd050c83428a0b70cb8fa1c132e2a5
RafieQt <tajuddinrafi76@gmail.com>
2026-05-01 23:46:23 +0600

    Implement registration form with validation and error handling

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,22 +1,61 @@
-
+"use client";
 import Link from "next/link";
 import authImg from "../assets/authenticationImg.png";
 import Image from "next/image";
+import { z } from "zod";
+import { FormEvent, useState } from "react";
+
+// ZOD schema for register
+const registerSchema = z.object({
+  username: z.string().min(1, "Username is short."),
+  fullname: z.string().min(1, "Full Name is short."),
+  email: z
+    .string("It is not a string.")
+    .email("Invalid Email.")
+    .min(1, "The email is too small."),
+  password: z.string().min(6, "Password must be 6 characters."),
+});
+
+type RegisterData = z.infer<typeof registerSchema>;
 
 export default function Register() {
+  const [username, setUsername] = useState("");
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+
+    const result = registerSchema.safeParse({
+      username,
+      fullname,
+      email,
+      password,
+    });
+
+    if (!result.success) {
+      setError(result.error.message);
+    }
+
+    console.log(result);
+  };
+
   return (
     <div className="bg-[#FAF7F2] h-screen">
-      <div className="bg-white w-7/12 mx-auto my-20 rounded-2xl h-110 p-5 px-8 shadow-2xl">
+      <div className="bg-white w-7/12 mx-auto my-20 rounded-2xl h-110 min-h-fit p-5 px-8 shadow-2xl">
         <h2 className="text-[#1F2937] text-3xl font-bold">Register</h2>
 
         <div className="flex justify-between">
-          <form className="flex flex-col ">
+          <form onSubmit={handleSubmit} className="flex flex-col ">
             {/* Username */}
             <label className="mt-3">Username:</label>
             <input
               className="border-1 rounded-xl p-1 w-80"
               type="text"
               placeholder="Your Username"
+              onChange={(e) => setUsername(e.target.value)}
             />
 
             {/* Full Name */}
@@ -25,6 +64,7 @@ export default function Register() {
               className="border-1 rounded-xl p-1 w-80"
               type="text"
               placeholder="Your Full Name"
+              onChange={(e) => setFullname(e.target.value)}
             />
 
             {/* Email */}
@@ -33,6 +73,7 @@ export default function Register() {
               className="border-1 rounded-xl p-1 w-80"
               type="email"
               placeholder="Your Email"
+              onChange={(e) => setEmail(e.target.value)}
             />
 
             {/* Password */}
@@ -41,15 +82,26 @@ export default function Register() {
               className="border-1 rounded-xl p-1 w-80"
               type="password"
               placeholder="Your Password"
+              onChange={(e) => setPassword(e.target.value)}
             />
-
-            <button className="bg-[#111827] text-white font-semibold text-center w-40 mt-3 rounded-xl py-1 mx-auto">
+            {error && <p>{error}</p>}
+            <button
+              type="submit"
+              className="bg-[#111827] text-white font-semibold text-center w-40 mt-3 rounded-xl py-1 mx-auto"
+            >
               Register
             </button>
-            <p className="text-center my-2">Already a User? <Link className="hover:cursor-pointer hover:underline" href='/Login'>Log In</Link> </p>
+            <p className="text-center my-2">
+              Already a User?{" "}
+              <Link
+                className="hover:cursor-pointer hover:underline"
+                href="/Login"
+              >
+                Log In
+              </Link>{" "}
+            </p>
           </form>
           <Image className="w-80 h-80" src={authImg} alt="" />
-          
         </div>
       </div>
     </div>
